654005603ca72f945c88092bffe2caf0c32fe26c
franklin anyaogu <fanyaogu1@byupathway.edu>
2026-05-14 02:36:08 +0100

    Added class stubs for week 02 Journal team activity

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,21 @@
+public class Journal
+{
+    public List<Entry> _entries;
+
+    public void DisplayAll()
+    {
+        
+    }
+    public void AddEntry(Entry newEntry)
+    {
+        
+    }
+    public void SaveToFile(string file)
+    {
+        
+    }
+    public void ReadFromFile(string file)
+    {
+        
+    }
+}
