bc4acc36f106a9169ad0bf3f07c2ef5fed83dc88
franklin anyaogu <fanyaogu1@byupathway.edu>
2026-05-21 15:49:33 +0100

    initial commit for scripturememorizer activity

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+       
+        Reference myReference = new Reference("Proverbs", 3, 5, 6);
+        Scripture myScripture = new Scripture(myReference, "Trust in the Lord with all thine heart; and lean not unto thine own understanding.");
+
+        myScripture.GetDisplayText();
+        myScripture.HideRandomWords(3);
+        myScripture.IsCompletelyHidden();
+
+        Console.WriteLine("All stubs are connected and working!");
     }
 }
